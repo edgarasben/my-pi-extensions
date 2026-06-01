0b9be070bb4b90b3a982cb9d7f6b591a7001f4d3
edgarasben <725324+edgarasben@users.noreply.github.com>
2026-06-01 11:55:06 +0300

    pi memory v1

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import memory from "./memory";
 import projects from "./projects";
 
 export default function myExtensions(pi: ExtensionAPI) {
 	projects(pi);
+	memory(pi);
 }
